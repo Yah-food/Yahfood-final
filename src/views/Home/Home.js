8f8fbe35e1295befe99aad42b4e6646aed853132
minoka kakizaki <minoka.kakizaki@gmail.com>
2022-01-18 16:54:25 -0800

    got the imgs to link to a new page

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import RecipeCard from '../../components/RecipeCard/RecipeCard';
 import { getAllRecipes } from '../../services/fetchData';
 
